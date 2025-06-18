0224284c9c21a326daac579095b35d65ae951670
akash balodi <107743958+akashbalodi@users.noreply.github.com>
2025-06-18 11:58:58 +0530

    Update Admin.js

--- clinic-backend/models/Admin.js
+++ clinic-backend/models/Admin.js
@@ -1,20 +1,3 @@
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
-
-// const adminSchema = new mongoose.Schema({
-//   email: { type: String, required: true },
-//   password: { type: String, required: true },
-// });
-
-// // Hash password before save
-// adminSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) return next();
-//   this.password = await bcrypt.hash(this.password, 10);
-//   next();
-// });
-
-// module.exports = mongoose.model('Admin', adminSchema);
-
 const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
